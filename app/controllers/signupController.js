191194312cd27ab2dbee15492950a34b5b061431
vijaypaliwal <vijay.paliwal@hotmail.com>
2015-05-15 12:20:17 +0530

    Update signupController.js

--- app/controllers/signupController.js
+++ app/controllers/signupController.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 app.controller('signupController', ['$scope', '$location', 'authService', 'ngAuthSettings', '$interval', 'localStorageService', 'log', function ($scope, $location, authService, ngAuthSettings, $interval, localStorageService, log) {
 
     $scope.savedSuccessfully = false;
@@ -25,7 +25,6 @@ app.controller('signupController', ['$scope', '$location', 'authService', 'ngAut
             $scope.savedSuccessfully = true;
             $scope.message = "User has been registered successfully, you will be redicted to dashboard page in 3 seconds.";
         
-            debugger;
 
             startTimer();
             $location.path('/graph');
@@ -38,10 +37,6 @@ app.controller('signupController', ['$scope', '$location', 'authService', 'ngAut
         },
          function (response) {
 
-
-             debugger;
-
-         
 
              if (response.status == "409") {
 
