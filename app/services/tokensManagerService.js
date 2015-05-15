954171fd270a350d7881026485e23278164896f7
vijaypaliwal <vijay.paliwal@hotmail.com>
2015-05-15 12:22:30 +0530

    Update tokensManagerService.js

--- app/services/tokensManagerService.js
+++ app/services/tokensManagerService.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 app.factory('tokensManagerService', ['$http','ngAuthSettings', function ($http,ngAuthSettings) {
 
     var serviceBase = ngAuthSettings.apiServiceBaseUri;
